811d50ccc3d5c3d2866d32a9bb09528a30eb165d
marcelocaldasdevops <marcelocaldasdevops@gmail.com>
2023-07-15 12:03:30 -0400

    iniciando

---
+++ src/main/java/com/gamedev/gamedev/GamedevApplication.java
@@ -0,0 +1,13 @@
+package com.gamedev.gamedev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamedevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamedevApplication.class, args);
+	}
+
+}
